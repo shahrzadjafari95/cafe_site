c1dc5985c234b2378c9657131fbcc2df70564e6c
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-07-22 00:25:58 +0330

    add name of apps blog and menu in the setting
    add name of apps blog and menu in the setting

--- Cafe/settings.py
+++ Cafe/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cafe_site'
+    'cafe_site',
+    'blog',
+    'menu',
+
 ]
 
 MIDDLEWARE = [
