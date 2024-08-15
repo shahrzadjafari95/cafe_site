38aef36937546418a18f17a8bb8df928e1646c15
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-15 12:49:50 +0330

    define class ContactAdmin for display customize fields in admin panel
    add date_hierarchy, list_display based on name, email and created_date , list_filter based on created_date and search_fields based on email, message and name.

--- cafe_site/admin.py
+++ cafe_site/admin.py
@@ -3,3 +3,9 @@ from cafe_site.models import Contact
 
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ['name', 'email', 'created_date']
+    list_filter = ['created_date']
+    search_fields = ['email', 'message', 'name']
+
