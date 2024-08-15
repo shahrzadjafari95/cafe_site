8ffb4fcc0bfd604df5407bee68fcb1385be7608f
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-15 12:35:16 +0330

    add date_hierarchy and empty_value_display

--- menu/admin.py
+++ menu/admin.py
@@ -7,3 +7,6 @@ class MenuAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'category', 'status', 'created_date', 'published_date']
     list_filter = ['category', 'status', 'created_date']
     search_fields = ['price', 'content']
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+
