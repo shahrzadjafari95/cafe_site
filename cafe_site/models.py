6fa5eae8a1f388ea15c8654f2b56b39a15503ef2
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-15 12:42:41 +0330

    add name, email, subject, message and created_date fields in Contact class

--- cafe_site/models.py
+++ cafe_site/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=False, blank=False)
+    subject = models.CharField(max_length=255, blank=False)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+
