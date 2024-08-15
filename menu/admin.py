ecd9a9a299e1f8841ae6857e08a8180cde3077a2
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-08-15 12:34:56 +0330

    add search_fields in MenuAdmin
    add search_fields based on price and content

--- menu/admin.py
+++ menu/admin.py
@@ -6,3 +6,4 @@ from menu.models import Menu, Category
 class MenuAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'category', 'status', 'created_date', 'published_date']
     list_filter = ['category', 'status', 'created_date']
+    search_fields = ['price', 'content']
