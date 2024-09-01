9428c99a0bb72a1c151cc3a77c79e413f6b763d8
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-09-01 21:27:32 +0330

    Filter out categories that don't have any available products

--- menu/views.py
+++ menu/views.py
@@ -12,3 +12,5 @@ def index(request):
     categories = Category.objects.prefetch_related(Prefetch('products', queryset=Product.objects.
         filter(status__in=('coming_soon', 'available'), published_date__lte=timezone.now()).order_by('name'))).order_by(
         'name')
+    # Filter out categories that don't have any available products
+    categories_with_products = [category for category in categories if category.products.exists()]
