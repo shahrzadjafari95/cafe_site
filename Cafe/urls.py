3993e54ffe03c43087c27604b78ad0d6a2660379
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-08 12:42:34 +0330

    Add URL path for Django Debug Toolbar

--- Cafe/urls.py
+++ Cafe/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     path('robots.txt', include('robots.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 
