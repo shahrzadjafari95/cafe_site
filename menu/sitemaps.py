8088e89920c1838ffc688aba9d5d694208fc608d
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-10-08 12:09:54 +0330

    Define the items() method to include all Product instances with a status of "available" and a published_date less than or equal to the current date, ensuring the sitemap reflects only relevant and published products.

--- menu/sitemaps.py
+++ menu/sitemaps.py
@@ -1,5 +1,15 @@
 from django.contrib.sitemaps import Sitemap
+from django.utils import timezone
+
+from menu.models import Product
+
+
 class MenuSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.5
+
+    def items(self):  # The items() method determines what data or objects will be included in the sitemap.
+
+        # This method queries the Post model to get all posts where the status is "A" and published_date less than now
+        return Product.objects.filter(status="available", published_date__lte=timezone.now())
 
